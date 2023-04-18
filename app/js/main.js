b95af3147727066bca2b378cda11f6a0faa208f5
schip <roziblanco454@gmail.com>
2023-04-18 20:29:27 +0300

    Сделал watch-maximum

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,56 @@
-$(function(){
+// Табы
+const tabs = (
+  headerSelector,
+  tabSelector,
+  contentSelector,
+  activeClass,
+  display = "block"
+) => {
+  const header = document.querySelector(headerSelector);
+  const tab = document.querySelectorAll(tabSelector);
+  const content = document.querySelectorAll(contentSelector);
 
+  function hideTabContent() {
+    content.forEach((item) => {
+      item.style.display = "none";
+    });
 
+    tab.forEach((item) => {
+      item.classList.remove(activeClass);
+    });
+  }
 
-})
+  function showTabContent(i = 0) {
+    content[i].style.display = display;
+
+    tab[i].classList.add(activeClass);
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  header.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (
+      target &&
+      (target.classList.contains(tabSelector.replace(/\./, "")) ||
+        target.parentNode.classList.contains(tabSelector.replace(/\./, "")))
+    ) {
+      tab.forEach((item, i) => {
+        if (target == item || target.parentNode == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+};
+
+tabs(
+  ".watch__info-box",
+  ".watch__info-button",
+  ".watch__info-content",
+  "active"
+);
+tabs(".watch__mobile", ".watch__info-button", ".watch__info-content", "active");
